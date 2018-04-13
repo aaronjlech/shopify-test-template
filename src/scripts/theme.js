1656c66ec483d976ac0e0af0cb97b8ddf0d33df1
Aaron LeCheminant <aaronjlech@gmail.com>
2018-04-13 08:28:03 -0400

    console logging cart

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -19,42 +19,56 @@ window.theme = window.theme || {};
 // =require templates/customers-login.js
 
 $(document).ready(function() {
-  var sections = new slate.Sections();
-  sections.register('product', theme.Product);
+    var sections = new slate.Sections();
+    sections.register("product", theme.Product);
 
-  // Common a11y fixes
-  slate.a11y.pageLinkFocus($(window.location.hash));
+    // Common a11y fixes
+    slate.a11y.pageLinkFocus($(window.location.hash));
 
-  $('.in-page-link').on('click', function(evt) {
-    slate.a11y.pageLinkFocus($(evt.currentTarget.hash));
-  });
+    $(".in-page-link").on("click", function(evt) {
+        slate.a11y.pageLinkFocus($(evt.currentTarget.hash));
+    });
 
-  // Target tables to make them scrollable
-  var tableSelectors = '.rte table';
+    // Target tables to make them scrollable
+    var tableSelectors = ".rte table";
 
-  slate.rte.wrapTable({
-    $tables: $(tableSelectors),
-    tableWrapperClass: 'rte__table-wrapper',
-  });
+    slate.rte.wrapTable({
+        $tables: $(tableSelectors),
+        tableWrapperClass: "rte__table-wrapper"
+    });
 
-  // Target iframes to make them responsive
-  var iframeSelectors =
-    '.rte iframe[src*="youtube.com/embed"],' +
-    '.rte iframe[src*="player.vimeo"]';
+    // Target iframes to make them responsive
+    var iframeSelectors =
+        '.rte iframe[src*="youtube.com/embed"],' +
+        '.rte iframe[src*="player.vimeo"]';
 
-  slate.rte.wrapIframe({
-    $iframes: $(iframeSelectors),
-    iframeWrapperClass: 'rte__video-wrapper'
-  });
+    slate.rte.wrapIframe({
+        $iframes: $(iframeSelectors),
+        iframeWrapperClass: "rte__video-wrapper"
+    });
 
-  // Apply a specific class to the html element for browser support of cookies.
-  if (slate.cart.cookiesEnabled()) {
-    document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies');
-  }
-  $('.js-menuDropdown').click(function(e) {
-      const menu = $(this).siblings('.menu__dropdown');
+    // Apply a specific class to the html element for browser support of cookies.
+    if (slate.cart.cookiesEnabled()) {
+        document.documentElement.className = document.documentElement.className.replace(
+            "supports-no-cookies",
+            "supports-cookies"
+        );
+    }
+    $(".js-menuDropdown").click(function(evt) {
+        const menu = $(this).siblings(".menu__dropdown");
 
-      menu.toggleClass('is--open');
+        menu.toggleClass("is--open");
+    });
+    $(".js-addToCart").submit(function(evt) {
+        evt.preventDefault();
+        console.log(evt.target);
+        const productId = $(this).data("productId");
 
-  })
+        // console.log(productId);
+        // CartJS.addItem(productId, quantity = 1, {
+        //     success: function(data) {
+        //         console.log(CartJS);
+        //     }
+        // });
+    })
 });
